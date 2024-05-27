b94a24fdb57ae93c20552bca76d0d2c59c83a0ef
TejuG1598 <tejaswisai.15@gmail.com>
2024-05-27 18:55:48 -0400

    Created html markup page using pug view engine

--- index.js
+++ index.js
@@ -2,10 +2,18 @@
 const express = require("express");
 const app = express();
 
+//set view engine to pug 
+app.set('view engine', 'pug');
+
+//set the path for the pug (this may be optional)
+app.set('views', './views' );
+
 //using the home router
 app.get("/",(req,res) =>{
-    res.send("Hello World! Welcome to vidly");
+    //res.send("Hello World! Welcome to vidly");
     //replace the above with html markup and return it to client first install pug 
+    res.render("index", { title: "My Vidly App", message: "Hello this is Tejaswi!" });
+
 })
 
 const port = process.env.PORT || 3000;

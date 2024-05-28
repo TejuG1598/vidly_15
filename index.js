b5958ae6d47d3561fc41fc170714f006026bdf66
TejuG1598 <tejaswisai.15@gmail.com>
2024-05-28 16:49:16 -0400

    Implemented the startup debugger

--- index.js
+++ index.js
@@ -8,13 +8,24 @@ app.set('view engine', 'pug');
 //set the path for the pug (this may be optional)
 app.set('views', './views' );
 
+// debug import function calling
+const startupDebugger = require("debug")("app:startup");
+
 //using the home router
 app.get("/",(req,res) =>{
     //res.send("Hello World! Welcome to vidly");
     //replace the above with html markup and return it to client first install pug 
     res.render("index", { title: "My Vidly App", message: "Hello this is Tejaswi!" });
 
-})
+});
+
+//our movie generes data
+const genres = [
+    { id: 1, genre: "Action" },
+    { id: 2, genre: "Horror" },
+    { id: 3, genre: "Romance" },
+  ];
+
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening on port ${port}...`));
+app.listen(port, () => startupDebugger(`Listening on port ${port}...`));
